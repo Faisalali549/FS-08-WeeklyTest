9cb1c0f67de8ae84176ab438ba4c21bad2f1cc31
Faisalali549 <99816472+Faisalali549@users.noreply.github.com>
2023-02-12 20:47:51 +0530

    Add files via upload

---
+++ universityEventManagement/src/main/java/com/example/universityEventManagement/model/Event.java
@@ -0,0 +1,15 @@
+package com.example.universityEventManagement.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class Event {
+    private int eventId;
+    private String eventName;
+    private String locationOfEvent;
+    private String date;
+    private String startTime;
+    private String endTime;
+}
